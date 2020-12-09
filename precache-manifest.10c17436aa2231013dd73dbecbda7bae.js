814fbf678691d6b5543ca9bd99168fe875dbb601
Ali Eskandari <alieskandari3@gmail.com>
2020-12-09 10:48:12 -0800

    Updates

---
+++ precache-manifest.10c17436aa2231013dd73dbecbda7bae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a320359273c4e0e286c4981af7538f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "349bb9d28b4f757218b4",
+    "url": "/static/css/main.25003be5.chunk.css"
+  },
+  {
+    "revision": "fac174727565ae06ccaa",
+    "url": "/static/js/2.c3ec8cec.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c3ec8cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349bb9d28b4f757218b4",
+    "url": "/static/js/main.b7ad4427.chunk.js"
+  },
+  {
+    "revision": "ae86936d9fd8aca7db51",
+    "url": "/static/js/runtime-main.562ba7cb.js"
+  }
+]);
